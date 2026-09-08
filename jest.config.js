81c5a6e6f2905e0f04ecf4af7a4eacdcb307dcb9
Tara L <taralaughton03@gmail.com>
2026-09-08 10:25:02 +0200

    Fixed path directory for jest configuration file from '<rootDir>' to './'

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 export default {
   testEnvironment: 'node',
-  setupFiles: ['<rootDir>/jest.setup.js'],
+  setupFiles: ['./jest.setup.js'],
   transform: {}, // disable Babel transforms
   testTimeout: 30000, // 60 seconds
 };
