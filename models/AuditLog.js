9821aa475b89818f75101ddc9052360bdc1ce388
Tara L <taralaughton03@gmail.com>
2026-09-09 10:56:54 +0200

    Added the missing 'signup' enum accepted type within the AuditLog schema

--- models/AuditLog.js
+++ models/AuditLog.js
@@ -8,7 +8,7 @@ const auditLogSchema = new mongoose.Schema({
   action: {
     type: String,
     enum: [
-      'login', 'logout', 'add-employee', 'remove-employee', 'edit-profile', 
+      'login', 'logout', 'signup', 'add-employee', 'remove-employee', 'edit-profile',
       'clock-in', 'clock-out',
       'tea-break-out', 'tea-break-in', 'lunch-break-out', 'lunch-break-in',
       'client-visit-out', 'client-visit-in',
