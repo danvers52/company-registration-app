cf5e96c8ff6350e86ebc06bfd6e9ee234aa0d381
Tara L <taralaughton03@gmail.com>
2026-09-10 19:27:30 +0200

    Changed environment back to 'development' and decided to code the 'production' within the render setup instead

--- utils/config.js
+++ utils/config.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const NODE_ENV = process.env.NODE_ENV?.trim() || 'production';
+const NODE_ENV = process.env.NODE_ENV?.trim() || 'development';
 const JWT_SECRET = process.env.JWT_SECRET?.trim();
 const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN?.trim() || '24h';
 const MONGODB_URI = process.env.MONGODB_URI?.trim() || 'mongodb://mongo:27017/company-registration?replicaSet=rs0';
